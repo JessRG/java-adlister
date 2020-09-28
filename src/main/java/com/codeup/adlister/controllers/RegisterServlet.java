71d03977701bc4672c2148c81190bb7d25bf5714
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-09-28 13:47:05 -0500

    A little progress through the password exercise, had to place imports into the MYSQL dao classes to use Config class. The password hashing still in progress.

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -2,6 +2,7 @@ package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.User;
+import org.mindrot.jbcrypt.BCrypt;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
